71fe53c16f14cac115ac406ad864e1df59f5b96e
Mask Tkachenko <MITkachenko@alfabank.ru>
2021-12-26 17:12:48 +0300

    feat: add save methods and logs

--- services/log.service.js
+++ services/log.service.js
@@ -1,6 +1,20 @@
 import chalk from "chalk";
 import dedent from "dedent";
 
+const successLog = (message) => {
+    console.log(dedent`${chalk.bgGreen(' SUCCESS ')}
+    ${chalk.blue(`Вас приветствует сервисный помошник`)}
+    Успех: ${message}
+    `)
+}
+
+const errorLog = (error) => {
+    console.log(dedent`${chalk.bgRed(' ERROR ')}
+    ${chalk.blue(`Вас приветствует сервисный помошник`)}
+    Ошибка: ${error.message}
+    `)
+}
+
 const helperLog = () => {
     console.log(dedent`${chalk.bgBlue(' HELP ')}
     ${chalk.blue(`Вас приветствует сервисный помошник`)}
@@ -10,4 +24,4 @@ const helperLog = () => {
     `)
 };
 
-export { helperLog };
+export { helperLog , errorLog, successLog };
--- wheather.js
+++ wheather.js
@@ -1,11 +1,26 @@
 import { getParams } from "./helper.js";
-import { helperLog } from "./services/log.service.js";
+import { errorLog, helperLog, successLog } from "./services/log.service.js";
+import { saveKeyValue, getValue } from "./services/storage.service.js";
+
+const setToken = async (token) => {
+     try {
+        await saveKeyValue('token', token);
+        const saveData = await getValue('token');
+        successLog(`Ваши данные (${saveData}) сохранены`);
+     } catch (error) {
+         errorLog(error);
+     }
+}
+
 
 const initApp = () => {
     const [executer, file, ...rest] = process.argv;
     const params = getParams(rest);
     if (params.h) {
         helperLog();
+    }
+    if (params.t) {
+        setToken(params.t)
     }
 };
 
